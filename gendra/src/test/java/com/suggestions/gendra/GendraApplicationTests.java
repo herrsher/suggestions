8077319b7fbb932e166554fcd4e27929fb7ebf76
herrsher <cgarcia.developer18@gmail.com>
2024-01-12 10:37:49 -0600

    spring boot intz

---
+++ gendra/src/test/java/com/suggestions/gendra/GendraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suggestions.gendra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GendraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
